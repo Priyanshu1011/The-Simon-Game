398ce890c0a7e9ba87e7533184ff2ab12bdeb34b
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2022-08-01 22:28:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,27 @@
+var gamePattern = [];
+var userClickedPattern = [];
 
-var keyPressedOnce = false;
+var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = buttonColours[nextSequence()]
+// console.log(randomChosenColour);
+gamePattern.push(randomChosenColour);
+console.log(gamePattern);
 
-$(document).keypress(function () {
-    if (!keyPressedOnce){
-        $("h2").text("Level 1");
-        var buttonNo = Math.floor(Math.random() * 4) + 1; // 1/2/3/4
-        displayAnimation(buttonNo);
-        keyPressedOnce = true;
-    }
-    
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+
+var sound = new Audio('sounds/' + randomChosenColour + '.mp3');
+sound.play();
+
+$(".btn").click(function(event){
+    var userChosenColour = event.target.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
 });
 
-function displayAnimation(buttonNo) {
-    switch (buttonNo) {
-        case 1:
-            $("#green-button").fadeOut();
-            setTimeout(function(){
-                $("#green-button").fadeIn();
-            }, 10);
-            break;
-    
-        case 2:
-            $("#red-button").fadeOut();
-            setTimeout(function(){
-                $("#red-button").fadeIn();
-            }, 10);
-            break;
-        
-        case 3:
-            $("#yellow-button").fadeOut();
-            setTimeout(function(){
-                $("#yellow-button").fadeIn();
-            }, 10);
-            break;
-        
-        case 4:
-            $("#blue-button").fadeOut();
-            setTimeout(function(){
-                $("#blue-button").fadeIn();
-            }, 10);
-            break;
-        
-        default:
-            break;
-    }
-}
+
+
